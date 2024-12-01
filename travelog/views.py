fad9db70a7badac8e960985d5e92a8a53a8d692c
Jisung Smart <jisung.smart@gmail.com>
2024-12-02 05:44:36 +0900

    Add another makre to map segwipo

--- travelog/views.py
+++ travelog/views.py
@@ -7,19 +7,27 @@ import folium
 
 # Create your views here.
 def travel_log(request):
-    # return HttpResponse('hello world')
-    shp_dir = os.path.join(os.getcwd(),'media','shp')
 
     #folium
     geocode = [33.3786,126.5662]
     m = folium.Map(location=[33.3786,126.5662], zoom_start=10)
+
+    seogwipo = [33.2532,126.5610]
     
-     #지도에 마커추가
+    #  #지도에 마커추가
     folium.Marker(
         location=geocode,
         popup=f'제주도',
         icon = folium.Icon(color='blue')
     ).add_to(m)
+
+
+    folium.Marker(
+        location=seogwipo,
+        popup=f'서귀포시',
+        icon = folium.Icon(color='blue')
+    ).add_to(m)
+
     
     # exporting
     map_html=m._repr_html_()
