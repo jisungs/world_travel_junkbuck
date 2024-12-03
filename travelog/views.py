4f2215a5498d7de8efd7bb8cbf6826f8e446199e
Jisung Smart <jisung.smart@gmail.com>
2024-12-04 05:40:21 +0900

    Add photo to folium map

--- travelog/views.py
+++ travelog/views.py
@@ -5,10 +5,20 @@ from django.shortcuts import render
 import os
 import folium
 
+import base64
+from folium import IFrame
+
 # Create your views here.
 def travel_log(request):
 
     #folium
+
+     #Add Marker
+    encoded = base64.b64encode(open('media/부산도심.jpg', 'rb').read())
+    html = '<img src="data:image/png;base64,{}">'.format
+    iframe = IFrame(html(encoded.decode('UTF-8')), width=100, height=100)
+    popup = folium.Popup(iframe, max_width=400)
+
     geocode = [33.3786,126.5662]
     m = folium.Map(location=[33.3786,126.5662], zoom_start=10)
 
@@ -17,13 +27,15 @@ def travel_log(request):
     #  #지도에 마커추가
     folium.Marker(
         location=geocode,
-        popup=f'제주도',
+        tooltip= html,
+        popup = popup, 
         icon = folium.Icon(color='blue')
     ).add_to(m)
 
 
     folium.Marker(
         location=seogwipo,
+        tooltip= html,
         popup=f'서귀포시',
         icon = folium.Icon(color='blue')
     ).add_to(m)
